c9a801e05dcd371e8406c8ccf1cd17820e0a0bc9
Radu Barsan <pub.retro@yahoo.com>
2024-11-20 18:10:44 +0200

    reparat js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ async function sendEmail(event) {
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
 
-    const response = await fetch('https://<API_GATEWAY_URL>/send-email', {
+    const response = await fetch('https://zja72hqpac.execute-api.us-east-1.amazonaws.com/sedmaildev', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name, email, message }),
